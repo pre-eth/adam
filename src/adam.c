62dd8bc1c3cdd59856b3c64d3a9d2cf2f3fdcc62
Preeth Vijay <preethvijay0@gmail.com>
2023-06-21 18:45:15 -0400

    accumulate creates an initial buffer of 1024 u8 so that 256 u64 numbers can be returned, 512 u32 numbers, etc... to make providing different precisions simple.
    diffuse then begins the algorithm designed in the paper, where a number of XOR operations occur across a large binary vector of the seed integers. each iteration after the initial one uses the last iteration as its starting point
    
    mod reduction as the comment describes is an implementation of an efficient modulus algorithm for large values described in "Computing Mod with a Variable Lookup Table by Mark A. Will and Ryan K. L. Ko"

---
+++ src/adam.c
@@ -0,0 +1,78 @@
+#include "adam.h"
+
+/*
+  To avoid expensive modulus for all the required chaotic iterations, this
+  function implements the modular reduction algorithm described in:
+
+ "Computing Mod with a Variable Lookup Table" by Mark A. Will & Ryan K. L. Ko 
+  from the Cyber Security Lab at The University of Waikato, New Zealand
+*/ 
+FORCE_INLINE static mod_reduction() {
+  #define WIDTH(w) (32 - CLZ(w))
+  
+}
+
+// Generates initial vector
+FORCE_INLINE static void accumulate(u8* restrict _ptr) {
+  #define SEED64    _rdseed64_step
+
+  u8 res;
+  u64 seed;
+  while (!(res = SEED64(&seed))); 
+  
+  seed ^= (seed ^ (GOLDEN_RATIO ^ (seed >> 32)));
+
+  u8 i = 0;
+  u8 j = 4;
+  
+  do {
+    ACCUMULATE(((seed + i) >> 56),  (i + 0)),
+    ACCUMULATE(((seed + i) >> 48),  (i + 16)),
+    ACCUMULATE(((seed + i) >> 40),  (i + 32)),
+    ACCUMULATE(((seed + i) >> 32),  (i + 48)),
+    ACCUMULATE(((seed + i) >> 24),  (i + 64)),
+    ACCUMULATE(((seed + i) >> 16),  (i + 80)),
+    ACCUMULATE(((seed + i) >> 8),   (i + 96)),
+    ACCUMULATE(((seed + i) & 0xFF), (i + 112));
+  } while (++i < 4);
+
+  do {
+    ACCUMULATE(((seed + j) >> 56),  (j + 0)),
+    ACCUMULATE(((seed + j) >> 48),  (j + 16)),
+    ACCUMULATE(((seed + j) >> 40),  (j + 32)),
+    ACCUMULATE(((seed + j) >> 32),  (j + 48)),
+    ACCUMULATE(((seed + j) >> 24),  (j + 64)),
+    ACCUMULATE(((seed + j) >> 16),  (j + 80)),
+    ACCUMULATE(((seed + j) >> 8),   (j + 96)),
+    ACCUMULATE(((seed + j) & 0xFF), (j + 112));
+  } while (++j < 8);
+}
+
+FORCE_INLINE static void diffuse(u8* restrict _ptr, float seed) {
+  /* 
+    According to the paper, the variable c is derived from: 
+      floor(log10(M)) + 3
+    Here M is the amount of bits in the buffer, which for ADAM is 
+    8192, and log10(8192) = 3.9, so c = floor(3.9) + 3 = 6. Then, 
+    BETA = pow(10, 6)
+  */
+  #define BETA          1000000 
+
+  float x = seed;
+  u16 s = SEQ_SIZE - 1;
+  u8 i, j;
+
+  s = SEQ_SIZE - 1;
+  i = j = 0;
+  do {
+    x = CHAOTIC_FN(x);
+    j = i + 1 + mod_reduction(FLOOR(x * BETA), s);
+    --s;
+    _ptr[i >> 3] |= (((_ptr[i >> 3] >> (i & 7)) & 1UL) ^ ((_ptr[j >> 3] >> (j & 7)) & 1UL)) << (i & 7);
+  } while (++i < BUF_SIZE - 2);
+}
+
+FORCE_INLINE void generate(u8* restrict _ptr, float seed) {
+  accumulate(_ptr);
+  diffuse(_ptr, seed);
+}
--- src/main.c
+++ src/main.c
@@ -8,9 +8,11 @@ int main(int argc, char** argv) {
   // You can imagine this as a multidimensional 8 arrays of u8 with size 1024.
   // This is to support the maximum ROUNDS count of 24, as each u64
   // stores values from a previous iteration of the algorithm  
-  u64 buffer[BUF_SIZE] ALIGN(32);
+  u8 buffer[BUF_SIZE] ALIGN(32);
+  
+  u8* restrict buf_ptr = &buffer;
 
-  generate(&buffer, seed);
+  generate(buf_ptr, seed);
   
   return 0;
 }
