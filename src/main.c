df61f07be81ad174c32b1dc051a8e893d93a0de8
Preeth Vijay <preethvijay0@gmail.com>
2023-06-28 14:27:58 -0400

    add getopt parsing logic to main.c and move the required imports from util.h to this file directly
    Not all of these functions are implemented yet, such as help(), stream_live, and stream_bits. The implementations exist in the original files but they haven't been properly converted and simplified to C code yet.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,53 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <sys/ioctl.h>
+
 #include "adam.h"
+#include "cli.h"
 
 int main(int argc, char** argv) {
-  float seed = DEFAULT_SEED;
-  u8 rounds = ROUNDS;
+  if (argc - 1 > ARG_MAX) 
+    return fputs("ERROR: Invalid number of arguments", stderr);
 
-  // TODO: GETOPT ARGPARSE LOGIC HERE
+  float seed = DEFAULT_SEED;
+  u8 rounds = ROUNDS, results = 0, precision = 8;
+
+  int opt;
+  while ((opt = getopt(argc, argv, OPTSTR)) != -1) {
+    switch (opt) {
+      case 'h':
+        return help();
+      case 'v':
+        return 
+        putchar('v'),
+        putchar('0' + MAJOR), putchar('.'),
+        putchar('0' + MINOR), putchar('.'),
+        putchar('0' + PATCH);  
+      case 'l':
+        return stream_live();
+      case 'b':
+        return stream_bits(__UINT64_MAX__);
+      case 'd':
+        results = 255;
+        break;
+      case 'n':
+        results = a_to_u(optarg, 0, 255);
+        break;
+      case 'r':
+        rounds = a_to_u(optarg, 8, 26) + 1;
+        break;
+      case 'p':
+        u8 p = a_to_u(optarg, 7, 63);
+        // Adapted from http://graphics.stanford.edu/~seander/bithacks.html#DetermineIfPowerOf2
+        if (p && !((p + 1) & p)) {
+          precision = p >> 3;
+          break;  
+        } else
+          return fputs("Precision must be either 8, 16, 32, or 64 bits.", stderr);    
+      default:
+        return fputs("Option is invalid or missing required argument.", stderr);             
+    }
+  }
 
   // You can imagine this as a multidimensional 4D array of u8 with size 1024.
   u32 buffer[BUF_SIZE] ALIGN(32);
