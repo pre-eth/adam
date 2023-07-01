3f2a664de793778bf40ebe632e53ca9c35d807e6
Preeth Vijay <preethvijay0@gmail.com>
2023-07-01 15:55:48 -0400

    add printing routine for generated number buffer, including the ability to print numbers of different precisions. the user can easily redirect this command from stdout to a file of their choice as well. Numbers are separated by a comma and space

--- src/main.c
+++ src/main.c
@@ -54,7 +54,49 @@ int main(int argc, char** argv) {
   
   u32* restrict buf_ptr = &buffer;
 
-  u64 seed = generate(buf_ptr, seed, rounds);
+  generate(buf_ptr, seed, rounds);
   
+  u16 idx = 0;
+
+  u64 num;
+  print_num:
+    num = 0;
+    switch (precision) {
+      case 1:
+        num  = buffer[idx] & 0xFF000000;
+        break;
+      case 2:
+        num  = ((buffer[idx] & 0xFF000000) << 8)
+             | (buffer[idx + 1] & 0xFF000000);
+        break;
+      case 4:
+        num  = ((buffer[idx] & 0xFF000000) << 24)
+             | ((buffer[idx + 1] & 0xFF000000) << 16)
+             | ((buffer[idx + 2] & 0xFF000000) << 8)
+             | (buffer[idx + 3] & 0xFF000000);
+        break;
+      case 8:
+        num  = ((buffer[idx] & 0xFF000000) << 56)
+             | ((buffer[idx + 1] & 0xFF000000) << 48)
+             | ((buffer[idx + 2] & 0xFF000000) << 40)
+             | ((buffer[idx + 3] & 0xFF000000) << 32)
+             | ((buffer[idx + 4] & 0xFF000000) << 24)
+             | ((buffer[idx + 5] & 0xFF000000) << 16)
+             | ((buffer[idx + 6] & 0xFF000000) << 8)
+             | (buffer[idx + 7] & 0xFF000000);
+        break;      
+    }
+    
+    idx += precision;
+
+    printf("%llu", num);
+
+    if (results-- > 0) {
+      printf(", ");
+      goto print_num;
+    }
+
+  putchar('\n');
+
   return 0;
 }
