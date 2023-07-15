0452f365571b867f9943d8b3daafde71e70c39df
Preeth Vijay <preethvijay0@gmail.com>
2023-07-14 20:02:11 -0400

    add STRINGIFY macros for creating version string at compile time
    refactor a_to_u to support up to 4 digits, because adam returns different amounts of numbers based on the precision, and a maximum of 2048 values can be returned if the user wants u8 precision
    
    even though the type signature has been expanded to u16 to account for this - 2048 is the hard ceiling value wise so parsing no more than 4 digits is okay

--- src/cli.h
+++ src/cli.h
@@ -1,8 +1,18 @@
 #ifndef CLI_H
 #define CLI_H
-  #define MAJOR       1
-  #define MINOR       0
-  #define PATCH       0
+
+  #include <stdio.h>
+  #include <getopt.h>
+  #include <sys/ioctl.h>
+
+  #define STRINGIZE1(a) #a
+  #define STRINGIFY1(a) STRINGIZE1(a)
+
+  #define MAJOR 1
+  #define MINOR 0
+  #define PATCH 0
+
+  #define VERSION "v" STRINGIFY1(MAJOR) "." STRINGIFY1(MINOR) "." STRINGIFY1(PATCH)
 
   #define OPTSTR      ":hvlbdn:r:p:a::s::u::"
   #define ARG_MAX     5
@@ -21,14 +31,18 @@
     return zeroes;
   }
 
-  FORCE_INLINE static u8 a_to_u(const char* s, u8 min, u8 max) {
-    u8 val, len = 0;
+  FORCE_INLINE static u16 a_to_u(const char* s, u16 min, u16 max) {
+    u8  len = 0;
+    u16 val = 0;
+    
+    // try while (len += (s[len] != '\0'));
     while (s[len++] != '\0');
 
     switch (len) { 
-      case  3:  val += (s[len- 3] - '0') * 100;
-      case  2:  val += (s[len- 2] - '0') * 10;
-      case  1:  val += (s[len- 1] - '0');
+      case  4: val += (s[len - 4]  - '0') * 1000;
+      case  3: val += (s[len - 3]  - '0') * 100;
+      case  2: val += (s[len - 2]  - '0') * 10;
+      case  1: val += (s[len - 1]  - '0');
         break;
     }
     return (val < min + 1 || val > max + 1) ? min : val;
