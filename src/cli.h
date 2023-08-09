31c28d62962e590ab6accebebe9ec3f29f484eb3
Preeth Vijay <preethvijay0@gmail.com>
2023-08-09 16:41:38 -0400

    align defs
    add declaration for stream_bits

--- src/cli.h
+++ src/cli.h
@@ -5,19 +5,25 @@
 
   #include "util.h"
 
-  #define STRINGIZE(a) #a
-  #define STRINGIFY(a) STRINGIZE(a)
+  #define STRINGIZE(a)  #a
+  #define STRINGIFY(a)  STRINGIZE(a)
 
-  #define MAJOR 1
-  #define MINOR 0
-  #define PATCH 0
+  #define MAJOR         1
+  #define MINOR         0
+  #define PATCH         0
 
-  #define VERSION "v" STRINGIFY(MAJOR) "." STRINGIFY(MINOR) "." STRINGIFY(PATCH)
+  #define VERSION       "v" STRINGIFY(MAJOR) "." STRINGIFY(MINOR) "." STRINGIFY(PATCH)
+  #define VERSION_HELP  "Version of this software (" STRINGIFY(MAJOR) "." STRINGIFY(MINOR) "." STRINGIFY(PATCH) ")"
 
-  #define OPTSTR      ":hvldn:r:p:a::b::u::"
-  #define ARG_MAX     5
-  #define ARG_COUNT   10
+  #define OPTSTR        ":hvldn:r:p:a::b::u::"
+  #define ARG_MAX       5
+  #define ARG_COUNT     10
 
-  u32 a_to_u(const char* s, const u32 min, const u32 max);
+  #define BITBUF_SIZE   512
+  #define ASSESS_MULT   1000000
+
+  u64 stream_bits(const u64 *restrict _ptr, const u64 limit);
+  u32 a_to_u(const char *s, const u32 min, const u32 max);
   u8 help();
+
 #endif
