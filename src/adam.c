32ed94f9ab972e59a195542e764613f27ce82bcb
Preeth Vijay <preethvijay0@gmail.com>
2023-08-09 12:33:13 -0400

    refactor accumulate to reduce code size and work with new buffer,
    processes 64 indices at a time

--- src/adam.c
+++ src/adam.c
@@ -41,30 +41,15 @@ FORCE_INLINE static double chaotic_iter(u64* map_b, u64* map_a, const double see
   return x;
 }
 
-FORCE_INLINE static void accumulate(u32* restrict _ptr, u64 seed) {
-  u8 i = 0, j = 4;
+FORCE_INLINE static void accumulate(u64* restrict _ptr, u64 seed) {
+  register u8 i = 0;
   
   do {
-    ACCUMULATE(((seed + i) >> 56),  (i + 0)),
-    ACCUMULATE(((seed + i) >> 48),  (i + 16)),
-    ACCUMULATE(((seed + i) >> 40),  (i + 32)),
-    ACCUMULATE(((seed + i) >> 32),  (i + 48)),
-    ACCUMULATE(((seed + i) >> 24),  (i + 64)),
-    ACCUMULATE(((seed + i) >> 16),  (i + 80)),
-    ACCUMULATE(((seed + i) >> 8),   (i + 96)),
-    ACCUMULATE(((seed + i) & 0xFF), (i + 112));
+    ACCUMULATE(((seed + (i << 6)) >> 56),  ((i << 6) + 0)),
+    ACCUMULATE(((seed + (i << 6)) >> 48),  ((i << 6) + 16)),
+    ACCUMULATE(((seed + (i << 6)) >> 40),  ((i << 6) + 32)),
+    ACCUMULATE(((seed + (i << 6)) >> 32),  ((i << 6) + 48));
   } while (++i < 4);
-
-  do {
-    ACCUMULATE(((seed + j) >> 56),  (j + 0)),
-    ACCUMULATE(((seed + j) >> 48),  (j + 16)),
-    ACCUMULATE(((seed + j) >> 40),  (j + 32)),
-    ACCUMULATE(((seed + j) >> 32),  (j + 48)),
-    ACCUMULATE(((seed + j) >> 24),  (j + 64)),
-    ACCUMULATE(((seed + j) >> 16),  (j + 80)),
-    ACCUMULATE(((seed + j) >> 8),   (j + 96)),
-    ACCUMULATE(((seed + j) & 0xFF), (j + 112));
-  } while (++j < 8);
 }
 
 FORCE_INLINE static void diffuse(u32* restrict _ptr, double* chseed, u8 iter) {
