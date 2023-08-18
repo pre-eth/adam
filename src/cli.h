cb16f980373ec7f0526239f9e91386aeb229192a
Preeth Vijay <preethvijay0@gmail.com>
2023-08-17 21:55:08 -0400

    update OPTSTR with most current set of options
    modify print_binary macro so it prints at proper offsets

--- src/cli.h
+++ src/cli.h
@@ -15,7 +15,7 @@
   #define VERSION       "v" STRINGIFY(MAJOR) "." STRINGIFY(MINOR) "." STRINGIFY(PATCH)
   #define VERSION_HELP  "Version of this software (" STRINGIFY(MAJOR) "." STRINGIFY(MINOR) "." STRINGIFY(PATCH) ")"
 
-  #define OPTSTR        ":hvldn:r:p:a::b::u::"
+  #define OPTSTR        ":hvldn:r:p:b::u::"
   #define ARG_MAX       5
   #define ARG_COUNT     10
 
@@ -24,10 +24,10 @@
 
   #define POPCNT_4(i)   POPCNT(_ptr[(i) + 0]) + POPCNT(_ptr[(i) + 1]) + POPCNT(_ptr[(i) + 2]) + POPCNT(_ptr[(i) + 3])
   #define PRINT_4(i, j) \
-    print_binary(_bptr + 64  + i, _ptr[j + 0]), \
-    print_binary(_bptr + 128 + i, _ptr[j + 1]), \
-    print_binary(_bptr + 192 + i, _ptr[j + 2]), \
-    print_binary(_bptr + 256 + i, _ptr[j + 3])  \
+    print_binary(_bptr + i,       _ptr[(j) + 0]), \
+    print_binary(_bptr + 64 + i,  _ptr[(j) + 1]), \
+    print_binary(_bptr + 128 + i, _ptr[(j) + 2]), \
+    print_binary(_bptr + 192 + i, _ptr[(j) + 3])  \
   
   u64 stream_bits(u64 *restrict _ptr, const u64 limit);
   u32 a_to_u(const char *s, const u32 min, const u32 max);
