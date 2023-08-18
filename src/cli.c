656083d0cf45c64ded94c259deb219a3a0044a2f
Preeth Vijay <preethvijay0@gmail.com>
2023-08-17 21:57:37 -0400

    finalize the logic for binary printing, for now. Only thing left to do is adding a FILE* pointer parameter so users can print the binary sequences as ASCII or bytes. Currently only ASCII is supported
    The -a option will handle printing both of those and write to a file for the user as convenience so they don't need to redirect stdout.

--- src/cli.c
+++ src/cli.c
@@ -12,30 +12,38 @@ static char bitbuffer[BITBUF_SIZE] ALIGN(64);
 
 // prints digits in reverse order to buffer
 FORCE_INLINE static void print_binary(char *restrict buf, u64 num) {
-  do *--buf = (num & 0x01) + '0';
+  char *bend = buf + 64;
+  *bend = '\0';
+  do *--bend = (num & 0x01) + '0';
   while (num >>= 1);
+
+  // If we reach the end of a number before printing 64 digits,
+  // fill in the rest with zeroes
+  while (bend - buf > 0)
+    *--bend = '0';
 }
 
 // prints all bits in a buffer as chunks of 1024 bits
-FORCE_INLINE static u16 print_chunks(char *restrict _bptr, const u64 *_ptr) {  
+FORCE_INLINE static u16 print_chunks(char *restrict _bptr, const u64 *restrict _ptr) {  
   register u8 i = 0;
   register u16 ones = 0;
 
   do {
-    ones += POPCNT_4(i + 0) + POPCNT_4(i + 4) + POPCNT_4(i + 8) + POPCNT_4(i + 12);
+    ones += POPCNT_4(i + 0) + POPCNT_4(i + 4) 
+          + POPCNT_4(i + 8) + POPCNT_4(i + 12);
 
-    PRINT_4(0, 0),
-    PRINT_4(256, 4),
-    PRINT_4(512, 8),
-    PRINT_4(768, 12);    
+    PRINT_4(0,   i + 0),
+    PRINT_4(256, i + 4),
+    PRINT_4(512, i + 8),
+    PRINT_4(768, i + 12);    
 
     fwrite(_bptr, 1, BITBUF_SIZE, stdout);
-  } while ((i += 16) < BUF_SIZE);
+  } while ((i += 16 - (i == 240)) < BUF_SIZE - 1);
 
   return ones;
 }
 
-u64 stream_bits(const u64 *restrict _ptr, const u64 limit) {
+u64 stream_bits(u64 *restrict _ptr, const u64 limit) {
   register u64 ones = 0;
 
   /*
@@ -44,14 +52,15 @@ u64 stream_bits(const u64 *restrict _ptr, const u64 limit) {
     an entire buffer (aka the SEQ_SIZE)
   */ 
   register short rate = limit >> 14;
-  register const short leftovers = limit & (SEQ_SIZE - 1);
+  register short leftovers = limit & (SEQ_SIZE - 1);
 
   char *restrict _bptr = &bitbuffer[0];
 
-  do {
+  while (rate > 0) {
     adam(_ptr);
     ones += print_chunks(_bptr, _ptr);
-  } while (--rate > 0);
+    --rate;
+  } 
 
   /*
     Since there are SEQ_SIZE (16384) bits in every 
@@ -63,35 +72,43 @@ u64 stream_bits(const u64 *restrict _ptr, const u64 limit) {
     if assessing bits, so this branch has been marked as LIKELY.
   */
   if (LIKELY(leftovers > 0)) {
-    // Remember - need to start at 64 since we print backwards
-    register short l = 64;
+    register short l;
+    register u16 limit;
+    register u64 num;
 
-    adam(_ptr);
-    do {
-      ones += POPCNT(*_ptr);
-      print_binary(_bptr + l, *_ptr++);
-    } while ((l += 64) < leftovers);
+    print_leftovers:
+      limit = (leftovers < BITBUF_SIZE) ? leftovers : BITBUF_SIZE;
 
-    fwrite(_bptr, 1, leftovers, stdout);
+      l = 0;
+      adam(_ptr);
+      do {
+        num = *(_ptr + (l >> 6));
+        ones += POPCNT(num);
+        print_binary(_bptr + l, num);
+      } while ((l += 64) < limit);
+
+      fwrite(_bptr, 1, limit, stdout);
+      leftovers -= limit;
+
+      if (LIKELY(leftovers > 0)) 
+        goto print_leftovers;
   }
 
   return ones;
 }
 
-// Only supports values up to 999,999
-u32 a_to_u(const char *s, const u32 min, const u32 max) {
+// Only supports values up to 4 digits
+u16 a_to_u(const char *s, const u16 min, const u16 max) {
   register u8  len = 0;
-  register u32 val = 0;
+  register u16 val = 0;
   
-  while (s[len++] != '\0');
+  for(; s[len] != '\0'; ++len);
 
   switch (len) { 
-    case  6: val += (s[len - 2]  - '0') * 100000;
-    case  5: val += (s[len - 1]  - '0') * 10000;
-    case  4: val += (s[len - 4]  - '0') * 1000;
-    case  3: val += (s[len - 3]  - '0') * 100;
-    case  2: val += (s[len - 2]  - '0') * 10;
-    case  1: val += (s[len - 1]  - '0');
+    case 4: val += (s[len - 4]  - '0') * 1000;
+    case 3: val += (s[len - 3]  - '0') * 100;
+    case 2: val += (s[len - 2]  - '0') * 10;
+    case 1: val += (s[len - 1]  - '0');
       break;
   }
   return (val < min + 1 || val > max + 1) ? min : val;
@@ -113,7 +130,7 @@ u8 help() {
   // max length for help description in COL 2 before it needs to wrap
   const u16 HELP_WIDTH = SWIDTH - HELP_INDENT;
 
-  const char ARGS[ARG_COUNT] = {'h', 'v', 'u', 'n', 'p', 'd', 'b', 'a', 's', 'l'};
+  const char ARGS[ARG_COUNT] = {'h', 'v', 'u', 'n', 'p', 'd', 'b', 'a', 'l'};
   const char *ARGSHELP[ARG_COUNT] = {
     "Get all available options",
     VERSION_HELP,
@@ -121,12 +138,11 @@ u8 help() {
     "Number of results to return (up to 256 u64, 512 u32, 1024 u16, or 2048 u8)",
     "Desired size (u8, u16, u32, u64) of returned numbers (default is u64)",
     "Dump the whole buffer",
-    "Just bits. Literally. You can provide a limit of N bits within [512, 999999]",
-    "Assess a sample of 1000000 bits (1 MB) written to a filename you provide. You can provide a multiplier within [1,256]",
-    "Set the seed. Optionally, returns the seed for the generated buffer",
+    "Just bits. Literally",
+    "Assess a binary or ASCII sample of 1000000 bits (1 MB) written to a filename you provide. You can choose a multiplier within [1,256]",
     "Live stream of continuously generated numbers"
   };
-  const u8 lengths[ARG_COUNT] = {25, 33, 108, 74, 69, 21, 76, 117, 67, 45};
+  const u8 lengths[ARG_COUNT] = {25, 33, 108, 74, 69, 21, 20, 132, 45};
   
   short len;
   for (int i = 0; i < ARG_COUNT; ++i) {
