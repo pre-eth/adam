639f4ca0a26aa7ab8ded45e360d2734c4c911d56
Preeth Vijay <preethvijay0@gmail.com>
2023-10-13 19:16:46 -0400

    interface change signals a minor version change
    update prototype for all functions that call adam()

--- src/cli.h
+++ src/cli.h
@@ -8,8 +8,8 @@
   #define STRINGIFY(a)    STRINGIZE(a)
 
   #define MAJOR           1
-  #define MINOR           3
-  #define PATCH           5
+  #define MINOR           4
+  #define PATCH           0
 
   #define VERSION         "v" STRINGIFY(MAJOR) "." STRINGIFY(MINOR) "." STRINGIFY(PATCH)
   #define VERSION_HELP    "Version of this software (" STRINGIFY(MAJOR) "." STRINGIFY(MINOR) "." STRINGIFY(PATCH) ")"
@@ -18,8 +18,8 @@
   #define ARG_COUNT       12
 
   #define BITBUF_SIZE     1024
-  #define ASSESS_BITS     1000000
-  #define ASSESS_LIMIT    4000
+  #define ASSESS_BITS     1000000ULL
+  #define ASSESS_LIMIT    8000
 
   #define PRINT_4(i, j)   print_binary(_bptr + i,       _ptr[(j) + 0]), \
                           print_binary(_bptr + 64 + i,  _ptr[(j) + 1]), \
@@ -30,11 +30,14 @@
   #define GET_2(i)        _ptr[i], _ptr[i + 1]
   #define GET_3(i)        _ptr[i], _ptr[i + 1], _ptr[i + 2]
 
-  u8  bits(u64 *restrict _ptr, u64 *seed, u64 *nonce);
-  u8  assess(u64 *restrict _ptr, const u16 limit, u64 *seed, u64 *nonce);
+  // Forward declaration - see adam.h for details
+  typedef struct rng_data rng_data;
+
+  u8  bits(rng_data *data);
+  u8  assess(const u16 limit, rng_data *data);
   u64 a_to_u(const char *s, const u64 min, const u64 max);
   u8  help();
-  u8  uuid(u64 *restrict _ptr, const u8 limit, u64 *seed, u64 *nonce);
+  u8  uuid(u8 limit, rng_data *data);
   u8  examine(u64 *restrict _ptr, const u16 limit, u64 *seed, u64 *nonce);
-  u8  infinite(u64 *restrict _ptr, u64 *seed, u64 *nonce);
+  u8  infinite(rng_data *data);
 #endif
