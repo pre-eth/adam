e7d472070c203d6a5318ca1df4456ed6b46b060c
Preeth Vijay <preethvijay0@gmail.com>
2024-02-06 12:20:33 -0500

    update logicb ased on changes to header, namely introduction of new options -d, -f, and -p
    update call syntax in uuid()
    
    create two static functions for printing an int or double

--- src/cli.c
+++ src/cli.c
@@ -40,6 +40,7 @@ static void print_summary(const u16 swidth, const u16 indent)
 
 u8 help()
 {
+#define ARG_COUNT 15
   u16 center, indent, swidth;
   get_print_metrics(&center, &indent, &swidth);
 
@@ -117,23 +118,43 @@ u8 set_width(rng_cli *cli, const char *strwidth)
     possible for this new precision in case it exceeds the possible limit
     This can be avoided by ordering your arguments so that -w comes first
   */
-  const u8 max = SEQ_SIZE >> CTZ(cli->width);
+  const u8 max = BUF_SIZE * (64 >> CTZ(cli->width));
   cli->results -= (cli->results > max) * (cli->results - max);
 
   return 0;
 }
 
+static void print_int(rng_cli *cli)
+{
+  u64 num;
+  adam_get(&num, cli->data, cli->width, NULL);
+  if (cli->hex)
+    printf("0x%llx", num);
+  else if (cli->octal)
+    printf("0o%llo", num);
+  else
+    printf("%llu", num);
+}
+
+static void print_dbl(rng_cli *cli)
+{
+  double d;
+  adam_get(&d, cli->data, cli->width, NULL);
+  printf("%.*lf", cli->precision, d);
+}
+
 u8 dump_buffer(rng_cli *cli)
 {
   rng_data *data = cli->data;
+  void (*write_fn)(rng_cli *cli) = (!data->dbl_mode) ? &print_int : &print_dbl;
 
-  printf(cli->fmt, adam_get(data, cli->width, NULL));
+  write_fn(cli);
 
   // index = 0 means old buffer has finished printing,
   // and new buffer has been generated.
   while (--cli->results > 0) {
     printf(",\n");
-    printf(cli->fmt, adam_get(data, cli->width, NULL));
+    write_fn(cli);
   }
 
   putchar('\n');
@@ -152,8 +173,12 @@ u8 uuid(const char *strlimit, rng_data *data)
 
   register u8 i = 0;
 
+  u64 lower, upper;
+
   do {
-    gen_uuid(adam_get(data, 64, NULL), adam_get(data, 64, NULL), &buf[0]);
+    adam_get(&upper, data, 64, NULL);
+    adam_get(&lower, data, 64, NULL);
+    gen_uuid(upper, lower, &buf[0]);
 
     // Print the UUID
     printf("%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%"
@@ -199,7 +224,7 @@ get_file_type:
   }
 
   fprintf(stderr,
-      "\n\033[0;1mWrote \033[36m%llu\033[m bits in \033[36m%lfs\033[m\n",
+      "\n\033[0;1mGenerated \033[36m%llu\033[m bits in \033[36m%lfs\033[m\n",
       total, duration);
 
   return 0;
@@ -211,12 +236,15 @@ u8 cli_runner(int argc, char **argv)
   rng_cli cli;
 
   adam_init(&data, false);
+  cli.results = 1;
   cli.data = &data;
-  cli.fmt = "%llu";
+  cli.hex = false;
+  cli.octal = false;
+  cli.precision = 15;
   cli.width = 64;
 
-  register short opt;
-  while ((opt = getopt(argc, argv, OPTSTR)) != -1) {
+  register char opt;
+  while ((opt = getopt(argc, argv, OPTSTR)) != EOF) {
     switch (opt) {
     case 'h':
       return help();
@@ -228,43 +256,41 @@ u8 cli_runner(int argc, char **argv)
       stream_bytes(__UINT64_MAX__, &data);
       return 0;
     case 'x':
-      cli.fmt = "0x%lX";
-      break;
+      cli.hex = true;
+      continue;
     case 'o':
-      cli.fmt = "0o%lo";
-      break;
+      cli.octal = true;
+      continue;
     case 'w':
       if (set_width(&cli, optarg))
         return err("Width must be either 8, 16, 32");
-      break;
+      continue;
     case 'r':
       // Returns all results if no argument provided
-      cli.results = (optarg == NULL) ? SEQ_SIZE >> CTZ(cli.width) : a_to_u(optarg, 1, SEQ_SIZE >> CTZ(cli.width));
+      cli.results = (optarg == NULL) ? BUF_SIZE * (64 / cli.width) : a_to_u(optarg, 1, BUF_SIZE * (64 / cli.width));
       if (cli.results == 0)
-        return err("Invalid number of results "
-                   "specified for desired width");
-      break;
+        return err("Invalid number of results specified for desired width");
+      continue;
     case 's':
       rwseed(&data.seed[0], optarg);
-      break;
+      continue;
     case 'n':
       rwnonce(&data.nonce, optarg);
-      break;
+      continue;
     case 'u':
       return uuid(optarg, &data);
     case 'd':
       data.dbl_mode = true;
-      cli.fmt = "%.*lf";
-      break;
+      continue;
     case 'f':
       data.dbl_mode = true;
-      cli.fmt = "%.*f";
       cli.width = 32;
       cli.precision = 7;
-      break;
+      continue;
     case 'p':
+      data.dbl_mode = true;
       cli.precision = a_to_u(optarg, 2, (cli.width == 32) ? 7 : 15);
-      break;
+      continue;
     case 'e':
       return examine(optarg, &data);
     default:
