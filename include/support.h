32f23d579d1f7ac0ac56d8979188819d79f17415
Preeth Vijay <preethvijay0@gmail.com>
2024-03-05 15:37:01 +0530

    the relationship between support.c and test.c has flipped - now test.c calls into support.c
    so we make the previously static print_x_results() functions public and have extra arguments for the distributions and such that are needed to print the examination findings properly
    
    remove get_seq_properties() due to the new relationship
    
    add some forward declarations so we can write the function prototypes properly

--- include/support.h
+++ include/support.h
@@ -6,8 +6,12 @@
 	
 	#define   TESTING_BITS    	        8000000ULL
 	#define   TESTING_DBL    		    1000ULL
-	#define   BITS_TESTING_LIMIT        100000
+	#define   BITS_TESTING_LIMIT        1000000
 	#define   DBL_TESTING_LIMIT         1500000
+
+	// Forward declarations - see test.h and ent.h
+	typedef struct rng_test rng_test;
+	typedef struct ent_test ent_test;
 
 	void     get_print_metrics(u16 *center, u16 *indent, u16 *swidth);
 	u8       err(const char *s);
@@ -15,9 +19,15 @@
 	u8       rwnonce(u64 *nonce, const char *strnonce);
 	u64      a_to_u(const char *s, const u64 min, const u64 max);
 	u8       gen_uuid(const u64 higher, const u64 lower, u8 *buf);
-	double 	 stream_ascii(const u64 limit, u64 *restrict buffer, u64 *restrict seed, u64 *restrict nonce);
-	double 	 dbl_ascii(const u32 limit, u64 *restrict buffer, u64 *restrict seed, u64 *restrict nonce, const u32 multiplier, const u8 precision);
-	double 	 stream_bytes(const u64 limit, u64 *restrict buffer, u64 *restrict seed, u64 *restrict nonce);
-	double	 dbl_bytes(const u32 limit, u64 *restrict buffer, u64 *restrict seed, u64 *restrict nonce, const u32 multiplier);
-	void 	 get_seq_properties(const u64 limit, unsigned long long *seed, unsigned long long nonce);
+	u8 		 nearest_space(const char *str, u8 offset);
+	void 	 print_ascii_bits(u64 *_ptr, const u64 limit);
+	void 	 print_basic_results(const u16 indent, const rng_test *rsl, const u64 limit);
+	void	 print_mfreq_results(const u16 indent, const rng_test *rsl);
+	void	 print_byte_results(const u16 indent, const rng_test *rsl, const u8 *restrict mcb, const u8 *restrict lcb);
+	void	 print_range_results(const u16 indent, const rng_test *rsl, const u64 *restrict range_dist);
+	void	 print_ent_results(const u16 indent, const ent_test *ent);
+	void	 print_chseed_results(const u16 indent, const u64 expected, const u64 *chseed_dist, double avg_chseed);
+	void	 print_fp_results(const u16 indent, const rng_test *rsl, const u64 *restrict fpf_dist, const u64 *restrict fpf_quad, const u64 *fp_perm_dist);
+	void	 print_avalanche_results(const u16 indent, const rng_test *rsl, const u64 *ham_dist);
+	void	 print_tbt_results(const u16 indent, const rng_test *rsl, const u64 *tbt_dist);
 #endif   
