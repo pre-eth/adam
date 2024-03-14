467c69e349d7780243fa00757ee15ff7d93b8c03
Preeth Vijay <preethvijay0@gmail.com>
2024-03-14 11:58:33 +0530

    include defines for implementing the Maurer Universal Test
    add necessary struct members for this test to rng_test
    
    rearrange some of the crypto test macros in order of bit-width
    
    remove Gamma defs as I am now using the gamma functions from the Cephes C math library, which will be explained more in the next commit
    
    add define for significance level = 0.01
    
    move MAX and MIN macros directly to test.h header from file

--- include/test.h
+++ include/test.h
@@ -5,13 +5,7 @@
   #include  "api.h"
 
   /*    NOTE: All critical values use an alpha level of 0.01    */
-
-  #define   GAMMA_2DF                 1
-  #define   GAMMA_4DF                 2
-  #define   GAMMA_5DF                 1.329340388179137
-  #define   GAMMA_8DF                 6
-  #define   GAMMA_10DF                24
-  #define   GAMMA_120DF               1.386831185456898E80
+  #define   ALPHA_LEVEL               0.01
 
   // FOR:   Range distribution of output values
   #define   RANGE_CAT                 5
@@ -42,13 +36,100 @@
   #define   FP_PERM_CRITICAL_VALUE    157.800
   #define   FP_PERM_PROB              (1.0 / 120.0)
 
-  // FOR:   Floating point max of 8 test
+  // FOR:   Floating point max of T test (T = 8)
   #define   FP_MAX_CAT                8
   #define   FP_MAX_PROB               0.125
   #define   FP_MAX_CRITICAL_VALUE     18.475
 
   /*
-    FOR:    Strict Avalanche Criterion (SAC) Test
+    FOR:   4-bit Saturation Point Test
+
+    The Saturation Point Test is similar to the Coupon Collector's Test, where it sees how long of a
+    sequence is required to get all the numbers in a set (in our case, 2^4 values), and logs the index 
+    where all such values appear (which is called the saturation point, or SP). It can be applied to
+    a variable width of integers and analyze short sequences as opposed to requiring longer sequences 
+    like some other tests here. The implementation differs a bit from what's described in the paper for 
+    efficiency's sake, including p-value calculation as the author passes a value of 2 to the igamc()
+    function used for calculating the p-value, where we can go one step further and directly pass 2.5
+    since we have a version that accepts double inputs. However, the test still works as expected and 
+    ADAM meets all the expected values, including hovering right around the expected SP (50 vs 51).
+
+    We check all 4-bit quantities per iteration (so basically ADAM_BUF_BYTES * 2), record the SP value
+    and then do a chi-square test for goodness of fit over the observed ranges of the SP values. The
+    probabilities were directly provided by the author in the paper.
+
+    Additionally, the raw chi-square, average SP, and overall distribution of values are reported.
+
+    F. Sulak, “A New Statistical Randomness Test: Saturation Point Test”, IJISS, vol. 2, no. 3,
+    pp. 81–85, 2013.
+  */
+  #define   SP_CAT                    5
+  #define   SP_DIST                   50
+  #define   SP_EXPECTED               51
+  #define   SP_K                      4
+  #define   SP_OBS_MIN                (1U << SP_K)
+  #define   SP_OBS_MAX                64
+  #define   SP_CRITICAL_VALUE         13.277
+  #define   SP_PROB1                  0.193609
+  #define   SP_PROB2                  0.179686
+  #define   SP_PROB3                  0.196007
+  #define   SP_PROB4                  0.195881
+  #define   SP_PROB5                  0.234818
+
+  /*
+    FOR:   8-bit Maurer Universal Statistical Test
+
+    The Maurer Universal Statistic is a great alternative/complement to other classic pseudorandom
+    statistical tests like the frequency, runs, autocorrelation, and serial tests, but offers 2 very
+    useful advantages over them. According to the paper:
+
+    1.  Rather than being tailored to detecting a specific type of statistical defect, the test is able 
+        to detect any one of the very general class of statistical defects that can be modeled by an
+        ergodic stationary source with finite memory, which includes all those detected by the tests
+        discussed in the previous section and can reasonably be argued to comprise the possible defects
+        that could realistically occur in a practical implementation of a random bit generator.
+
+    2.  The test measures the actual amount by which the security of a cipher system would be reduced if
+        the tested generator G were used as the key source, i.e., it measures the effective key size of a
+        cipher system with key source G. Therefore, statistical defects are weighted according to the
+        potential damage they would cause in a cryptographic application. 
+
+    The reason why I included this test is to provide more in-house tests for judging the cryptographic
+    qualities of ADAM specifically, as opposed to some of the other tests which are just property reporting
+    and traditional PRNG tests that don't necessarily deduce anything about security.
+  */
+  #define   MAURER_ARR_SIZE           1001472
+  #define   MAURER_L                  8
+  #define   MAURER_Q                  2048
+  #define   MAURER_Y                  2.58
+  #define   MAURER_EXPECTED           7.1836656 
+  #define   MAURER_VARIANCE           3.2386622
+
+  /*
+    FOR:   16-bit Topological Binary Test (TBT)
+
+    The 16-bit TBT looks for the number of distinct 16-bit patterns per 2^16 patterns, where a "pattern"
+    is a 16-bit block of bits. Since we provide 1024 such patterns per iteration (each iteration of
+    ADAM produces 1024 u16), we count the number of distinct 16-bit patterns over 64 RNG iterations. If
+    the number of distinct numbers found is greater than or equal to TBT_CRITICAL_VALUE, then we fail to 
+    reject the null hypothesis.
+
+    The pass rate, average number of distinct patterns, and overall proportion are reported in addition
+    to the target metrics from the original paper. While the paper specifies the discrete probability 
+    function allowing us to obtain a p-value, actually using the function for calculations proves very
+    difficult due to the large values involved, especially the Stirling Numbers of the Second Kind. So
+    only the collected results of each discrete outcome is reported.
+
+    Alcover, Pedro & Guillamón, Antonio & Ruiz, M.D.C.. (2013). A New Randomness Test for Bit Sequences.
+    Informatica (Netherlands). 24. 339-356. 10.15388/Informatica.2013.399.
+  */ 
+  #define   TBT_M                     16
+  #define   TBT_SEQ_SIZE              (1UL << TBT_M)
+  #define   TBT_CRITICAL_VALUE        41241
+  #define   TBT_PROPORTION            0.629
+
+  /*
+    FOR:    64-bit Strict Avalanche Criterion (SAC) Test
 
     64 different probabilities needed here so instead of polluting the header, see the function
     print_avalanche_results() in support.c.
@@ -79,42 +160,6 @@
   #define   AVALANCHE_CAT             4
   #define   AVALANCHE_CRITICAL_VALUE  11.345
 
-  /*
-    FOR:   16-bit Topological Binary Test (TBT)
-
-    The 16-bit TBT looks for the number of distinct 16-bit patterns per 2^16 patterns, where a "pattern"
-    is a 16-bit block of bits. Since we provide 1024 such patterns per iteration (each iteration of
-    ADAM produces 1024 u16), we count the number of distinct 16-bit patterns over 64 RNG iterations. If
-    the number of distinct numbers found is greater than or equal to TBT_CRITICAL_VALUE, then we fail to 
-    reject the null hypothesis.
-
-    The pass rate, average number of distinct patterns, and overall proportion are reported in addition
-    to the target metrics from the original paper. While the paper specifies the discrete probability 
-    function allowing us to obtain a p-value, actually using the function for calculations proves very
-    difficult due to the large values involved, especially the Stirling Numbers of the Second Kind. So
-    only the collected results of each discrete outcome is reported.
-
-    Alcover, Pedro & Guillamón, Antonio & Ruiz, M.D.C.. (2013). A New Randomness Test for Bit Sequences.
-    Informatica (Netherlands). 24. 339-356. 10.15388/Informatica.2013.399.
-  */ 
-  #define   TBT_M                     16
-  #define   TBT_SEQ_SIZE              (1UL << TBT_M)
-  #define   TBT_CRITICAL_VALUE        41241
-  #define   TBT_PROPORTION            0.629
-
-  #define   SP_CAT                    5
-  #define   SP_DIST                   50
-  #define   SP_EXPECTED               51
-  #define   SP_K                      4
-  #define   SP_OBS_MIN                16
-  #define   SP_OBS_MAX                64
-  #define   SP_CRITICAL_VALUE         13.277
-  #define   SP_PROB1                  0.193609
-  #define   SP_PROB2                  0.179686
-  #define   SP_PROB3                  0.196007
-  #define   SP_PROB4                  0.195881
-  #define   SP_PROB5                  0.234818
-
   typedef struct rng_test {
     u64 init_values[5];
     u64 sequences;
@@ -133,13 +178,22 @@
     u64 longest_zero;
     double avg_fp;
     u64 perms;
-    u16 *tbt_array;
-    u64 tbt_pass;
-    u64 tbt_prop;
+    u64 *tbt_array;
     double avg_chseed;
-    u64 expected_chseed;
     u64 fp_max_runs;
+    double maurer_stat;
+    u8 *maurer_bytes;
+    u64 maurer_pass;
+    double maurer_mean;
+    double maurer_c;
+    double maurer_std_dev;
+    double maurer_fisher;
   } rng_test;
 
+  // https://graphics.stanford.edu/~seander/bithacks.html#IntegerMinOrMax
+  #define MIN(a, b) (b ^ ((a ^ b) & -(a < b)))
+  #define MAX(a, b) (a ^ ((a ^ b) & -(a < b)))
+
   void adam_examine(const u64 limit, adam_data data);
+  double cephes_igamc(double a, double x);
 #endif
