f6409eca22bcfd92b1129218a94e4a49935df510
Preeth Vijay <preethvijay0@gmail.com>
2024-07-15 17:53:13 -0400

    update other methods based on new RNG scheme and adam_int()

--- src/api.c
+++ src/api.c
@@ -95,176 +95,63 @@ u64 adam_int(adam_data data, const NumWidth width)
     return generate(data->out, &data->buff_idx, data->chseeds) & WIDTH_MASK(width);
 }
 
-double adam_dbl(adam_data data, const u64 scale, const bool force_regen)
+double adam_dbl(adam_data data, const u64 scale)
 {
-    return (double) (scale + !scale) * ((double) adam_int(data, 64, force_regen) / (double) __UINT64_MAX__);
+    return (double) (scale + !scale) * ((double) adam_int(data, UINT64) / (double) __UINT64_MAX__);
 }
 
-int adam_fill(adam_data data, void *buf, u8 width, const u64 amount)
+int adam_fill(adam_data data, void *buf, const NumWidth width, const size_t amount)
 {
     if (!amount || amount > ADAM_FILL_MAX) {
         return 1;
     }
 
-    if (width != 8 && width != 16 && width != 32 && width != 64) {
-        width = 64;
+    const size_t total = amount * width;
+    
+    register size_t written = 0;
+
+    u8 *_ptr = (u8 *) buf;
+    u64 tmp;
+
+    while (total - written >= ADAM_WORD_SIZE) {
+        tmp = adam_int(data, UINT64);
+        MEMCPY(&_ptr[written], &tmp, ADAM_WORD_SIZE);
+        written += ADAM_WORD_SIZE;
     }
 
-    // Determine the divisor based on width and bit shifting
-    const u16 one_run   = (7 + (64 / width));
-    const u32 leftovers = amount & ((1UL << one_run) - 1);
-
-    register long long runs = amount >> one_run;
-
-    while (runs > 0) {
-        adam(data);
-        MEMCPY(buf, data->out, ADAM_BUF_BYTES);
-        buf += ADAM_BUF_BYTES;
-        --runs;
+    if (total - written > 0) {
+        tmp = adam_int(data, UINT64);
+        MEMCPY(&_ptr[written], &tmp, total - written);
     }
-
-    if (LIKELY(leftovers > 0)) {
-        adam(data);
-        MEMCPY(buf, data->out, leftovers);
-    }
-
-    // To force regeneration on next API call for fresh buffer
-    data->buff_idx = ADAM_BUF_BYTES;
 
     return 0;
 }
 
-static void dbl_simd_fill(double *buf, u64 *restrict _ptr, const u16 amount)
-{
-    register u16 i = 0;
-
-#ifdef __AARCH64_SIMD__
-    const dregq range = SIMD_SETQPD(1.0 / (double) __UINT64_MAX__);
-
-    reg64q4 r1;
-    dreg4q d1;
-
-    do {
-        r1        = SIMD_LOAD64x4(&_ptr[i]);
-        d1.val[0] = SIMD_MULPD(SIMD_CASTPD(r1.val[0]), range);
-        d1.val[1] = SIMD_MULPD(SIMD_CASTPD(r1.val[1]), range);
-        d1.val[2] = SIMD_MULPD(SIMD_CASTPD(r1.val[2]), range);
-        d1.val[3] = SIMD_MULPD(SIMD_CASTPD(r1.val[3]), range);
-        SIMD_STORE4PD(&buf[i], d1);
-    } while ((i += 8) < amount);
-#else
-    const regd range = SIMD_SETPD(1.0 / (double) __UINT64_MAX__);
-
-    reg r1;
-    regd d1;
-
-    do {
-#ifdef __AVX512F__
-        r1 = SIMD_LOADBITS((reg *) &_ptr[i]);
-        d1 = SIMD_CASTPD(r1);
-        d1 = SIMD_MULPD(d1, range);
-        SIMD_STOREPD(&buf[i], d1);
-#else
-        r1 = SIMD_LOADBITS((reg *) &_ptr[i]);
-        d1 = SIMD_CVTPD(r1);
-        d1 = SIMD_MULPD(d1, range);
-        SIMD_STOREPD(&buf[i], d1);
-
-        r1 = SIMD_LOADBITS((reg *) &_ptr[i + 4]);
-        d1 = SIMD_CVTPD(r1);
-        d1 = SIMD_MULPD(d1, range);
-        SIMD_STOREPD(&buf[i + 4], d1);
-#endif
-    } while ((i += 8) < amount);
-#endif
-}
-
-static void dbl_simd_mult(double *buf, const u16 amount, const double multiplier)
-{
-    register u16 i = 0;
-
-#ifdef __AARCH64_SIMD__
-    const dregq mult = SIMD_SETQPD(multiplier);
-    dreg4q d1;
-
-    do {
-        d1 = SIMD_LOAD4PD(&buf[i]);
-        SIMD_MUL4QPD(d1, d1, mult);
-        SIMD_STORE4PD(&buf[i], d1);
-    } while ((i += 8) < amount);
-#else
-    const regd mult = SIMD_SETPD(multiplier);
-    regd d1;
-
-    do {
-        d1 = SIMD_LOADPD(&buf[i]);
-        d1 = SIMD_MULPD(d1, mult);
-        SIMD_STOREPD(&buf[i], d1);
-
-#ifndef __AVX512F__
-        d1 = SIMD_LOADPD(&buf[i + 4]);
-        d1 = SIMD_MULPD(d1, mult);
-        SIMD_STOREPD(&buf[i + 4], d1);
-#endif
-    } while ((i += 8) < amount);
-#endif
-}
-
-int adam_dfill(adam_data data, double *buf, const u64 multiplier, const u64 amount)
+int adam_dfill(adam_data data, double *buf, const u64 multiplier, const size_t amount)
 {
     if (!amount || amount > ADAM_FILL_MAX) {
         return 1;
     }
 
-    register u32 count = 0;
-    register u32 out;
-    register u32 tmp = amount - count;
+    const double mult = multiplier + !multiplier;
 
-    while (tmp >= 8) {
-        adam(data);
-        out = ((tmp > BUF_SIZE) << 8) | (!(tmp > BUF_SIZE) * tmp);
-        dbl_simd_fill(&buf[count], data->out, out);
-        count += out;
-        tmp = amount - count;
-    }
+    register size_t count = 0;
 
-    const u8 leftovers = amount & 7;
-
-    if (LIKELY(leftovers)) {
-        adam(data);
-        register u8 i = 0;
-        do {
-            buf[count] = (double) data->out[i++] / (double) __UINT64_MAX__;
-        } while (++count < amount);
-    }
-
-    if (multiplier > 1) {
-        const double mult = multiplier;
-        if (amount >= 8) {
-            dbl_simd_mult(buf, amount, mult);
-        }
-
-        if (LIKELY(leftovers)) {
-            count = amount - leftovers;
-            do {
-                buf[count] *= mult;
-            } while (++count < amount);
-        }
-    }
-
-    data->buff_idx = ADAM_BUF_BYTES;
+    do {
+        buf[count] = mult * ((double) adam_int(data, UINT64) / (double) __UINT64_MAX__);
+    } while (++count < amount);
 
     return 0;
 }
 
-void *adam_choice(adam_data data, void *arr, const u64 size)
+void *adam_choice(adam_data data, void *arr, const size_t size)
 {
-    return &arr[adam_int(data, 64, false) % size];
+    return &arr[adam_int(data, UINT64) % size];
 }
 
-u64 adam_stream(adam_data data, const u64 output, const char *file_name)
+size_t adam_stream(adam_data data, const size_t amount, const char *file_name)
 {
-    if (output < ADAM_BUF_BITS) {
+    if (amount < ADAM_WORD_BITS) {
         return 0;
     }
 
@@ -272,23 +159,13 @@ u64 adam_stream(adam_data data, const u64 output, const char *file_name)
         freopen(file_name, "wb+", stdout);
     }
 
-    const u16 leftovers = output & (ADAM_BUF_BITS - 1);
-
-    register u64 written = 0;
-    while (output - written >= ADAM_BUF_BITS) {
-        adam(data);
-        fwrite(data->out, sizeof(u64), BUF_SIZE, stdout);
-        written += ADAM_BUF_BITS;
+    register size_t written = 0;
+    u64 tmp;
+    while (amount - written >= ADAM_WORD_BITS) {
+        tmp = adam_int(data, UINT64);
+        fwrite(&tmp, ADAM_WORD_SIZE, 1, stdout);
+        written += ADAM_WORD_BITS;
     }
-
-    if (LIKELY(leftovers)) {
-        adam(data);
-        fwrite(data->out, sizeof(u64), leftovers >> 6, stdout);
-        written += leftovers;
-    }
-
-    // To force regeneration on next API call for fresh buffer
-    data->buff_idx = ADAM_BUF_BYTES;
 
     return written;
 }
